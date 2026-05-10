116146c408d76ce1876a723a84b9b35c76e55beb
y13028037686 <13028037686@163.com>
2026-05-10 11:07:07 +0800

    Update user_api.py

--- api/user_api.py
+++ api/user_api.py
@@ -1,1 +1,30 @@
+from common.api_client import ApiClient
 
+
+class UserAPI:
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def register(self, username: str, password: str):
+        return self.client.post(
+            "/user/register",
+            json_body={
+                "username": username,
+                "password": password
+            }
+        )
+
+    def login(self, username: str, password: str):
+        return self.client.post(
+            "/user/login",
+            json_body={
+                "username": username,
+                "password": password
+            }
+        )
+
+    def get_info(self):
+        return self.client.get("/user/info")
+
+    def logout(self):
+        return self.client.post("/user/logout")
