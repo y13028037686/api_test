96ffda52aac99af631c4b5568989515543676181
y13028037686 <13028037686@163.com>
2026-05-10 11:06:51 +0800

    Update post_api.py

--- api/post_api.py
+++ api/post_api.py
@@ -1,1 +1,30 @@
+from common.api_client import ApiClient
 
+
+class PostAPI:
+    def __init__(self, client: ApiClient):
+        self.client = client
+
+    def create(self, title: str, content: str):
+        return self.client.post(
+            "/post",
+            json_body={
+                "title": title,
+                "content": content
+            }
+        )
+
+    def update(self, post_id: int, title: str, content: str):
+        return self.client.put(
+            f"/post/{post_id}",
+            json_body={
+                "title": title,
+                "content": content
+            }
+        )
+
+    def detail(self, post_id: int):
+        return self.client.get(f"/post/{post_id}")
+
+    def delete(self, post_id: int):
+        return self.client.delete(f"/post/{post_id}")
